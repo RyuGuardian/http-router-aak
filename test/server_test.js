fce3c226edafd08a95b2970e210f5df147106446
Austin King <aust.a.king@gmail.com>
2015-09-13 15:18:24 -0700

    Made it work!

--- test/server_test.js
+++ test/server_test.js
@@ -4,7 +4,8 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 var expect = chai.expect;
-require(__dirname + '/../server');
+require(__dirname + '/../lib/server');
+var fs = require('fs');
 
 var host = 'localhost:3000';
 
@@ -30,35 +31,14 @@ describe('HTTP server', function() {
       });
   });
 
-  it("should greet for named urls", function(done) {
+  it("should save body in JSON file", function(done) {
     chai.request(host)
-      .get('/greet/saul')
+      .post('/body')
+      .send({hello: 'world'})
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(res.status).to.eql(200);
-        expect(res.text).to.eql('Hello, saul');
-        done();
-      });
-  });
-
-  it("should not like numbers (and other symbols)", function(done) {
-    chai.request(host)
-      .get('/greet/345')
-      .end(function(err, res) {
-        expect(err).to.eql(null);
-        expect(res.status).to.eql(400);
-        expect(res.text).to.eql('Not a valid name. Nice try.');
-        done();
-      });
-  });
-
-  it("should not take urls that aren't blank or start with 'greet' or 'time'", function(done) {
-    chai.request(host)
-      .get('/dfbiwerh8')
-      .end(function(err, res) {
-        expect(err).to.eql(null);
-        expect(res.status).to.eql(404);
-        expect(res.text).to.eql('Error 404\nPage not found.');
+        expect(fs.existsSync(__dirname + '/../data/request' + (fs.readdirSync(__dirname + '/../data').length - 1) + '.json')).to.eql(true);
         done();
       });
   });
